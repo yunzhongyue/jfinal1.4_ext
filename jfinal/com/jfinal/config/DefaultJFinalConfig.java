b6c2007677eaaa047bd46218cfd25b2596371b59
zhangyanlin <zhangyanlin3@yeah.net>
2013-05-08 20:48:51 +0800

    jfinal1.4衍生版version1.0
    相对于jfinal1.4的改变有：
    1.去掉了jfinal内嵌服务器的支持.
    2.程序默认实现了一个JFinalConfig的子类，框架不再要求实现一个JFinalConfig的子类。
    在jfinal过滤器未配置configClass的情况下，默任JFinalConfig将生效。
    3.可以选择在配置jfinal过滤器时，通过初始化参数配置jfinal常量。如devMode,viewType等。
    
    4.无需手工注册Action，程序将自动将所有继承Controller进行注册。此功能默认开启，如果要手工注册，请在jfinal过滤器的启动参数中将autoRegisterAction设置为false（默认注册的Action,其key为类的全限定名，可通过BasePath注解和ActionKey注解分别为类和方法指定路径，如果不这样做，该类将不可访问。）
    5.修改默认配置，将默认devMode设置为true，viewTyp设置为jsp

---
+++ jfinal/com/jfinal/config/DefaultJFinalConfig.java
@@ -0,0 +1,36 @@
+package com.jfinal.config;
+
+
+/**
+ * 缺省jfinal配置类
+ * @author zhang
+ *
+ */
+public class DefaultJFinalConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants me) {
+
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+		
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+
+	}
+
+}
---
+++ jfinal/com/jfinal/core/JFinal.java
@@ -0,0 +1,243 @@
+/**
+ * Copyright (c) 2011-2012, James Zhan 詹波 (jfinal@126.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jfinal.core;
+
+import java.io.File;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.handler.Handler;
+import com.jfinal.handler.HandlerFactory;
+import com.jfinal.i18n.I18N;
+import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.IPlugin;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.render.RenderFactory;
+import com.jfinal.token.ITokenCache;
+import com.jfinal.token.TokenManager;
+import com.jfinal.upload.OreillyCos;
+
+/**
+ * JFinal
+ */
+public final class JFinal {
+	
+	public static final String DEFAULT_JFINAL_CFG="com.jfinal.config.DefaultJFinalConfig";
+	
+	private Constants constants;
+	private ActionMapping actionMapping;
+	private Handler handler;
+	private ServletContext servletContext;
+	private String contextPath = "";
+	
+	Handler getHandler() {
+		return handler;
+	}
+	
+	private static final JFinal me = new JFinal();
+	
+	private JFinal() {
+	}
+	
+	public static JFinal me() {
+		return me;
+	}
+	
+	boolean init(JFinalConfig jfinalConfig, ServletContext servletContext) {
+		this.servletContext = servletContext;
+		this.contextPath = servletContext.getContextPath();
+		
+		
+		
+		initPathUtil();
+
+		Config.configJFinal(jfinalConfig);	// start plugin and init logger factory in this method
+		
+		constants = Config.getConstants();
+		if(constants.isAutoRegisterAction()) initRoute();
+		
+		initActionMapping();
+		initHandler();
+		initRender();
+		initActiveRecord();
+		initOreillyCos();
+		initI18n();
+		initTokenManager();
+		
+		return true;
+	}
+	
+	private void initRoute()
+	{
+		String classPath=PathKit.getRootClassPath();
+		File file=new File(classPath);
+		try {
+			scanRoutes(file,"");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void scanRoutes(File dir,String pkgPath) throws ClassNotFoundException
+	{
+		for(File file:dir.listFiles())
+		{
+			if(file.isDirectory())
+			{
+				scanRoutes(file,pkgPath+file.getName()+".");
+			}
+			else if(file.getName().indexOf(".class")!=-1)
+			{
+				String fileName=null;
+				
+				if(pkgPath.indexOf('.')==-1)
+				{
+					fileName=file.getName();
+				}
+				else
+				{
+					fileName=pkgPath+file.getName();
+				}
+				
+				fileName=fileName.substring(0, fileName.indexOf(".class"));
+				
+				Class<?> clazz=Class.forName(fileName);
+				
+				if(Controller.class.isAssignableFrom(clazz))
+				{
+					BasePath basePath=clazz.getAnnotation(BasePath.class);
+					if(basePath!=null)
+					{
+						String basePathVal=basePath.value().trim();
+						if("".equals(basePathVal))
+						{
+							throw new IllegalArgumentException(clazz.getName() + ":The argument of BasePath can not be blank.");
+						}
+						else
+						{
+							Config.getRoutes().add(basePathVal, (Class<? extends Controller>) clazz);
+						}
+					}
+					else
+					{
+						Config.getRoutes().add(fileName, (Class<? extends Controller>) clazz);
+					}
+				}
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	
+	private void initTokenManager() {
+		ITokenCache tokenCache = constants.getTokenCache();
+		if (tokenCache != null)
+			TokenManager.init(tokenCache);
+	}
+	
+	private void initI18n() {
+		String i18nResourceBaseName = constants.getI18nResourceBaseName();
+		if (i18nResourceBaseName != null) {
+			I18N.init(i18nResourceBaseName, constants.getI18nDefaultLocale(), constants.getI18nMaxAgeOfCookie());
+		}
+	}
+	
+	private void initHandler() {
+		Handler actionHandler = new ActionHandler(actionMapping, constants);
+		handler = HandlerFactory.getHandler(Config.getHandlers().getHandlerList(), actionHandler);
+	}
+	
+	private void initOreillyCos() {
+		Constants ct = constants;
+		if (OreillyCos.isMultipartSupported()) {
+			String uploadedFileSaveDirectory = ct.getUploadedFileSaveDirectory();
+			if (uploadedFileSaveDirectory == null || "".equals(uploadedFileSaveDirectory.trim())) {
+				uploadedFileSaveDirectory = PathKit.getWebRootPath() + File.separator + "upload" + File.separator;
+				ct.setUploadedFileSaveDirectory(uploadedFileSaveDirectory);
+				
+				/*File file = new File(uploadedFileSaveDirectory);
+				if (!file.exists())
+					file.mkdirs();*/
+			}
+			OreillyCos.init(uploadedFileSaveDirectory, ct.getMaxPostSize(), ct.getEncoding());
+		}
+	}
+	
+	private void initActiveRecord() {
+		ActiveRecordPlugin.setDevMode(constants.getDevMode());
+	}
+	
+	private void initPathUtil() {
+		String path = servletContext.getRealPath("/");
+		PathKit.setWebRootPath(path);
+	}
+	
+	private void initRender() {
+		RenderFactory renderFactory = RenderFactory.me();
+		renderFactory.init(constants, servletContext);
+	}
+	
+	private void initActionMapping() {
+		actionMapping = new ActionMapping(Config.getRoutes(), Config.getInterceptors());
+		actionMapping.buildActionMapping();
+	}
+	
+	void stopPlugins() {
+		List<IPlugin> plugins = Config.getPlugins().getPluginList();
+		if (plugins != null) {
+			for (int i=plugins.size()-1; i >= 0; i--) {		// stop plugins
+				boolean success = false;
+				try {
+					success = plugins.get(i).stop();
+				} 
+				catch (Exception e) {
+					success = false;
+					e.printStackTrace();
+				}
+				if (!success) {
+					System.err.println("Plugin stop error: " + plugins.get(i).getClass().getName());
+				}
+			}
+		}
+	}
+	
+	public ServletContext getServletContext() {
+		return this.servletContext;
+	}
+	
+	public List<String> getAllActionKeys() {
+		return actionMapping.getAllActionKeys();
+	}
+	
+	public Constants getConstants() {
+		return Config.getConstants();
+	}
+	
+	public Action getAction(String url, String[] urlPara) {
+		return actionMapping.getAction(url, urlPara);
+	}
+	
+	public String getContextPath() {
+		return contextPath;
+	}
+}
